1c0a808169322fb9b5d00d1cb31994a0e332bf44
Chase Lee <chaseklee990@gmail.com>
2020-11-10 13:57:29 -0800

    Implment basic auth on client

---
+++ client/pages/index.js
@@ -0,0 +1,18 @@
+import buildClient from '../api/build-client';
+
+const LandingPage = ({ currentUser }) => {
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are not signed in</h1>
+  );
+};
+
+LandingPage.getInitialProps = async (ctx) => {
+  const client = buildClient(ctx);
+  const { data } = await client.get('/api/users/currentuser');
+
+  return data;
+};
+
+export default LandingPage;
