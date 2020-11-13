fee4040b7029d7c5431fdb9adb5ab81fde23e858
Chase Lee <chaseklee990@gmail.com>
2020-11-12 16:02:37 -0800

    Update @ckl/common

--- common/src/middlewares/error-handler.ts
+++ common/src/middlewares/error-handler.ts
@@ -5,13 +5,15 @@ export const errorHandler = (
   err: Error,
   req: Request,
   res: Response,
-  next: NextFunction
-  ) => {
-    if (err instanceof CustomError) {
-      return res.status(err.statusCode).send({ errors: err.serializeErrors() });
-    }
+  next: NextFunction,
+) => {
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+  }
 
-    res.status(400).send({
-      errors: [{ message: 'Something went wrong' }]
-    });
+  console.error(err);
+
+  res.status(400).send({
+    errors: [{ message: 'Something went wrong' }],
+  });
 };
